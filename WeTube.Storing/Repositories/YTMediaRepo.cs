41d1cb6c647f9c728b6169acc4f7a2b0fc0b7f1e
Dallas Ward <dallaspward@gmail.com>
2019-12-10 19:14:10 -0600

    add ytmediarepo

---
+++ WeTube.Storing/Repositories/YTMediaRepo.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using WeTube.Domain.Models;
+
+namespace WeTube.Storing.Repositories
+{
+  public class YTMediaRepo
+  {
+    private List<DVideo> _YTMediaLibrary = new List<DVideo>();
+    public WeTubeContext wTContext = new WeTubeContext();
+
+    public List<DVideo> YTMediaLibrary
+    {
+      get
+      {
+        return _YTMediaLibrary;
+      }
+    }
+
+    public YTMediaRepo()
+    {
+      Initialize();
+    }
+
+    public List<DVideo> Initialize()
+    {
+      if(_YTMediaLibrary == null)
+      {
+        _YTMediaLibrary = new List<DVideo>();
+
+        //populate from database
+      }
+
+      return _YTMediaLibrary;
+    }
+
+    public List<DVideo> AddVideo(DVideo newVideo)
+    {
+      _YTMediaLibrary.Add(newVideo);
+
+      //push to database
+
+      return _YTMediaLibrary;
+    }
+  }
+}

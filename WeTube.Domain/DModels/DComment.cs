c7240197a49ef8ebb76c308530ba3fa9b9215762
lyndsay-durbin <lyndsaydurbin@yahoo.com>
2019-12-16 09:35:43 -0600

    deleting unnecessaries

--- WeTube.Domain/DModels/DComment.cs
+++ WeTube.Domain/DModels/DComment.cs
@@ -6,20 +6,6 @@ namespace WeTube.Domain.DModels
 {
    public class DComment
    {
-      //a comment should have a time variable that is used to keep track of the moment it was named
-
-      //if tyler passes the comment through as a string then I'll receive it as a string:
-      //string comment;
-      //each comment needs to have a time associated with it, this can be the number of seconds since receiving it?
-      //DateTime time = DateTime.Now;
-      //string TimeReceived = DateTime.Now.ToString("h:mm:ss tt"); //this takes the time passed since midnight (would put it in a normal time stamp)
-      //taking a user account name
-      //string user; //the username of the person making the comment
-
-      //create a comment and timereceived array? would also need to track the user who made the comment
-
-
-
       //strictly created from the database
       //public int Id { get; set; }
       public DateTime TimeReceived { get; set; }
@@ -46,6 +32,12 @@ namespace WeTube.Domain.DModels
         commentsMade.Add(practiceComment);
 
         return commentsMade;
+      }
+
+      public List<DComment> DCommentAt(DateTime received)
+      {
+        List<DComment> commentsAt = new List<DComment>();
+        return commentsAt;
       }
    }
 }
